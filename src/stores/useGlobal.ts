08251b81a934eda7474f26a8191ae8b9852d0777
lopo <lopo@zju.edu.cn>
2022-06-24 23:04:47 +0800

    feat: elder-mode

--- src/stores/useGlobal.ts
+++ src/stores/useGlobal.ts
@@ -4,14 +4,19 @@ export const useGlobal = defineStore({
     id: 'global',
     state() {
         return {
-            theme: (localStorage.getItem('theme') ?? 'light') as 'light' | 'dark'
-
+            theme: (localStorage.getItem('theme') ?? 'light') as 'light' | 'dark',
+            elderMode: (localStorage.getItem('elderMode') ?? 'disable') as 'enable' | 'disable'
         }
     },
     actions: {
         toggleTheme() {
             this.theme = this.theme === 'light' ? 'dark' : 'light'
             localStorage.setItem('theme', this.theme)
+        },
+        toggleElderMode() {
+            this.elderMode = this.elderMode === 'enable' ? 'disable' : 'enable'
+            document.documentElement.className = 'elder-' + this.elderMode
+            localStorage.setItem('elderMode', this.elderMode)
         }
     }
 })
