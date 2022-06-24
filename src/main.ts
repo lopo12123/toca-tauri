5e7d61cf37dfd2fdc253d0b8c63d7e0834b12a18
chenmh <chenmh@sucsoft.com>
2022-06-24 17:49:30 +0800

    chore(deps): element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 // style
+import "element-plus/dist/index.css";
 import "@/styles/light_dark.scss";
 import "@/styles/index.scss";
 
@@ -6,11 +7,13 @@ import "@/styles/index.scss";
 import { createApp } from "vue";
 
 // plugin
+import ElementPlus from "element-plus";
 import { createPinia } from "pinia";
 import { router } from "@/routers";
 import App from "./App.vue";
 
 createApp(App)
+    .use(ElementPlus)
     .use(createPinia())
     .use(router)
     .mount('#app')
