0967af696652c56b20e8016ddaff849549a0b206
lopo <lopo@zju.edu.cn>
2022-06-24 21:37:16 +0800

    chore: icon and base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     clearScreen: false,
     plugins: [ vue() ],
     envPrefix: [ 'VITE_', 'TAURI_' ],
