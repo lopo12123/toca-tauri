f2796932005f5c73f629d584afb0fa7517155323
lopo <lopo@zju.edu.cn>
2022-06-25 15:24:42 +0800

    shake: drop useless

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,19 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
-mod app_banner;
 mod toca;
 
-use tauri::{Builder, command, generate_context, generate_handler};
-use app_banner::*;
+use tauri::{Builder, generate_context};
 
 fn main() {
-    let context = generate_context!();
     Builder::default()
         // ipc 命令
-        .invoke_handler(generate_handler![maximum_window, minimum_window, exit_app])
+        // .invoke_handler(generate_handler![])
+
         // 创建一个空的菜单列表则菜单自动隐藏
         .menu(tauri::Menu::with_items(vec![]))
-        .run(context)
-        .expect("error while running tauri application");
-}
 
-#[command]
-fn greet(msg: &str) {
-    println!("msg from front-end: {}", msg);
+        // 运行 tauri.conf.json 配置信息
+        .run(generate_context!())
+        .expect("error while running tauri application");
 }
