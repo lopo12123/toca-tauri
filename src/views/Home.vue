40b0c878c7678bab8338ecad3d5e201b51d1cfbd
chenmh <chenmh@sucsoft.com>
2022-06-27 14:31:17 +0800

    refactor: root url

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="home">
+        home
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
