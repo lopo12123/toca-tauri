c3f04ea4577610af8803a5cea340706a612f441b
lopo <lopo@zju.edu.cn>
2022-06-28 14:51:20 +0800

    feat: 首页选择跳转

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,47 @@
 <script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { ElMessage } from "element-plus";
 
+const router = useRouter()
+const selectOption = (type: 'display' | 'record') => {
+    if(type === 'display') {
+        router.push({ name: 'TimelineViewer' })
+    }
+    else if(type === 'record') {
+        ElMessage({
+            type: 'info',
+            message: '暂未实现'
+        })
+    }
+}
 </script>
 
 <template>
     <div class="home">
-        home
+        <div class="option-btn" @click="selectOption('display')">播放</div>
+        <div class="option-btn" @click="selectOption('record')">录制</div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use "src/styles/mixin";
+
 .home {
     position: relative;
     width: 100%;
     height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .option-btn {
+        @include mixin.pointer-hover;
+        width: 5rem;
+        height: 3rem;
+        border: solid 1px var(--separator-stroke);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 }
 </style>
