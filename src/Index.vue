9b542fcb7129bb6bdfece2a7dd9de556d755811d
lopo <lopo@zju.edu.cn>
2022-07-11 10:57:48 +0800

    refactor: remove app banner

--- src/Index.vue
+++ src/Index.vue
@@ -1,44 +1,26 @@
 <script lang="ts" setup>
-import AppBanner from "@/layouts/AppBanner.vue";
-import { useGlobal } from "@/stores/useGlobal";
-import { invoke } from "@tauri-apps/api";
-import { ref } from "vue";
-
-const global = useGlobal()
-
-const record_status = ref<string>('wating')
-const doKeyRecord = () => {
-    record_status.value = 'recording'
-    invoke('record_keyboard', { signal: 'Escape' })
-        .then((res: any) => {
-            record_status.value = 'done'
-            console.log(res)
-        })
-        .catch((err: any) => {
-            record_status.value = 'error'
-            console.log(err)
-        })
-}
+// const record_status = ref<string>('waiting')
+// const doKeyRecord = () => {
+//     record_status.value = 'recording'
+//     invoke('record_keyboard', { signal: 'Escape' })
+//         .then((res: any) => {
+//             record_status.value = 'done'
+//             console.log(res)
+//         })
+//         .catch((err: any) => {
+//             record_status.value = 'error'
+//             console.log(err)
+//         })
+// }
 </script>
 
 <template>
-    <div :class="['index', global.theme]">
-        <div class="status">
-            status: <span>{{ record_status }}</span>
-        </div>
-        <!--        <div class="app-banner-container">-->
-        <!--            <AppBanner/>-->
-        <!--        </div>-->
-        <!--        <div class="app-view-container">-->
-        <!--            <router-view/>-->
-        <!--        </div>-->
-        <button @click="doKeyRecord">record keys</button>
+    <div class="index global">
+        <router-view/>
     </div>
 </template>
 
 <style lang="scss" scoped>
-$banner-width: 2rem;
-
 .index {
     position: relative;
     width: calc(100vw - 2px);
@@ -49,18 +31,5 @@ $banner-width: 2rem;
     display: flex;
     align-items: center;
     justify-content: center;
-
-    .app-banner-container {
-        position: relative;
-        width: $banner-width;
-        height: 100vh;
-        border-right: solid 1px var(--separator-stroke);
-    }
-
-    .app-view-container {
-        position: relative;
-        width: calc(100% - #{$banner-width} - 1px);
-        height: 100vh;
-    }
 }
 </style>
