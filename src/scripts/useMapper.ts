3487d7c056bd737e5dcf77d7f398dcda21ab359d
lopo <lopo@zju.edu.cn>
2022-07-12 15:57:11 +0800

    feat: 按键值  名称  映射

---
+++ src/scripts/useMapper.ts
@@ -0,0 +1,323 @@
+type SignalKey = {
+    code: string
+    name: string
+}
+
+const ValidSignalKey: SignalKey[] = [
+    // region F1-F12
+    {
+        "code": "F1",
+        "name": "F1"
+    },
+    {
+        "code": "F2",
+        "name": "F2"
+    },
+    {
+        "code": "F3",
+        "name": "F3"
+    },
+    {
+        "code": "F4",
+        "name": "F4"
+    },
+    {
+        "code": "F5",
+        "name": "F5"
+    },
+    {
+        "code": "F6",
+        "name": "F6"
+    },
+    {
+        "code": "F7",
+        "name": "F7"
+    },
+    {
+        "code": "F8",
+        "name": "F8"
+    },
+    {
+        "code": "F9",
+        "name": "F9"
+    },
+    {
+        "code": "F10",
+        "name": "F10"
+    },
+    {
+        "code": "F11",
+        "name": "F11"
+    },
+    {
+        "code": "F12",
+        "name": "F12"
+    },
+    // endregion
+    // region number 0-9
+    {
+        "code": "Digit0",
+        "name": "数字0"
+    },
+    {
+        "code": "Digit1",
+        "name": "数字1"
+    },
+    {
+        "code": "Digit2",
+        "name": "数字2"
+    },
+    {
+        "code": "Digit3",
+        "name": "数字3"
+    },
+    {
+        "code": "Digit4",
+        "name": "数字4"
+    },
+    {
+        "code": "Digit5",
+        "name": "数字5"
+    },
+    {
+        "code": "Digit6",
+        "name": "数字6"
+    },
+    {
+        "code": "Digit7",
+        "name": "数字7"
+    },
+    {
+        "code": "Digit8",
+        "name": "数字8"
+    },
+    {
+        "code": "Digit9",
+        "name": "数字9"
+    },
+    // endregion
+    // region key A-Z
+    {
+        "code": "KeyA",
+        "name": "按键A"
+    },
+    {
+        "code": "KeyB",
+        "name": "按键B"
+    },
+    {
+        "code": "KeyC",
+        "name": "按键C"
+    },
+    {
+        "code": "KeyD",
+        "name": "按键D"
+    },
+    {
+        "code": "KeyE",
+        "name": "按键E"
+    },
+    {
+        "code": "KeyF",
+        "name": "按键F"
+    },
+    {
+        "code": "KeyG",
+        "name": "按键G"
+    },
+    {
+        "code": "KeyH",
+        "name": "按键H"
+    },
+    {
+        "code": "KeyI",
+        "name": "按键I"
+    },
+    {
+        "code": "KeyJ",
+        "name": "按键J"
+    },
+    {
+        "code": "KeyK",
+        "name": "按键K"
+    },
+    {
+        "code": "KeyL",
+        "name": "按键L"
+    },
+    {
+        "code": "KeyM",
+        "name": "按键M"
+    },
+    {
+        "code": "KeyN",
+        "name": "按键N"
+    },
+    {
+        "code": "KeyO",
+        "name": "按键O"
+    },
+    {
+        "code": "KeyP",
+        "name": "按键P"
+    },
+    {
+        "code": "KeyQ",
+        "name": "按键Q"
+    },
+    {
+        "code": "KeyR",
+        "name": "按键R"
+    },
+    {
+        "code": "KeyS",
+        "name": "按键S"
+    },
+    {
+        "code": "KeyT",
+        "name": "按键T"
+    },
+    {
+        "code": "KeyU",
+        "name": "按键U"
+    },
+    {
+        "code": "KeyV",
+        "name": "按键V"
+    },
+    {
+        "code": "KeyW",
+        "name": "按键W"
+    },
+    {
+        "code": "KeyX",
+        "name": "按键X"
+    },
+    {
+        "code": "KeyY",
+        "name": "按键Y"
+    },
+    {
+        "code": "KeyZ",
+        "name": "按键Z"
+    },
+    // endregion
+    // region function keys
+    {
+        "code": "Escape",
+        "name": "Escape"
+    },
+    {
+        "code": "Tab",
+        "name": "Tab"
+    },
+    {
+        "code": "CapsLock",
+        "name": "CapsLock"
+    },
+    {
+        "code": "ShiftLeft",
+        "name": "Shift"
+    },
+    {
+        "code": "AltLeft",
+        "name": "Alt"
+    },
+    {
+        "code": "Space",
+        "name": "Space"
+    },
+    {
+        "code": "ArrowUp",
+        "name": "方向↑"
+    },
+    {
+        "code": "ArrowRight",
+        "name": "方向→"
+    },
+    {
+        "code": "ArrowDown",
+        "name": "方向↓"
+    },
+    {
+        "code": "ArrowLeft",
+        "name": "方向←"
+    },
+    {
+        "code": "Enter",
+        "name": "Enter"
+    },
+    {
+        "code": "Backspace",
+        "name": "Backspace"
+    },
+    {
+        "code": "Delete",
+        "name": "Delete"
+    },
+    {
+        "code": "Home",
+        "name": "Home"
+    },
+    {
+        "code": "PageUp",
+        "name": "PageUp"
+    },
+    {
+        "code": "PageDown",
+        "name": "PageDown"
+    },
+    {
+        "code": "End",
+        "name": "End"
+    },
+    {
+        "code": "Backquote",
+        "name": "反引号 `"
+    },
+    {
+        "code": "Minus",
+        "name": "减号 -"
+    },
+    {
+        "code": "Equal",
+        "name": "等号 ="
+    },
+    {
+        "code": "BracketLeft",
+        "name": "左括号 ["
+    },
+    {
+        "code": "BracketRight",
+        "name": "右括号 ]"
+    },
+    {
+        "code": "Comma",
+        "name": "逗号 ,"
+    },
+    {
+        "code": "Period",
+        "name": "句号 ."
+    },
+    {
+        "code": "Semicolon",
+        "name": "分号 ;"
+    },
+    {
+        "code": "Quote",
+        "name": "引号 '"
+    },
+    {
+        "code": "Slash",
+        "name": "斜杠 /"
+    },
+    {
+        "code": "BackSlash",
+        "name": "反斜杠 \\"
+    }
+    // endregion
+]
+
+export {
+    ValidSignalKey
+}
