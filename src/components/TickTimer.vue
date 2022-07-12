1403711d882680d249a3cbda3ac9486ae8dfe972
lopo <lopo@zju.edu.cn>
2022-07-12 14:14:02 +0800

    feat: LEC ticker

---
+++ src/components/TickTimer.vue
@@ -0,0 +1,116 @@
+<script lang="ts" setup>
+import { onBeforeUnmount, onMounted, ref } from "vue";
+
+export type TimerHandle = {
+    start: () => void,
+    stop: () => void
+}
+
+const emits = defineEmits<{
+    (ev: 'tickReady', handle: TimerHandle): void
+}>()
+
+const hour = ref(0)
+const min = ref(0)
+const sec = ref(0)
+const dot_sec = ref(0)
+
+const isTiming = ref(false)
+const timerId = ref<any>(-1)
+const startTick = () => {
+    // 重置
+    hour.value = 0
+    min.value = 0
+    sec.value = 0
+    dot_sec.value = 0
+
+    timerId.value = setInterval(() => {
+        if(dot_sec.value < 9) dot_sec.value += 1
+        // 进位 1s
+        else {
+            dot_sec.value = 0
+
+            if(sec.value < 59) sec.value += 1
+            // 进位 1min
+            else {
+                sec.value = 0
+
+                if(min.value < 59) min.value += 1
+                // 进位 1hour
+                else {
+                    min.value = 0
+
+                    hour.value = hour.value + 1
+                }
+            }
+        }
+    }, 100)
+    isTiming.value = true
+}
+const stopTick = () => {
+    clearInterval(timerId.value)
+    isTiming.value = false
+}
+
+onMounted(() => {
+    emits('tickReady', { start: startTick, stop: stopTick })
+})
+onBeforeUnmount(() => {
+    stopTick()
+})
+</script>
+
+<template>
+    <div class="tick-timer">
+        <div class="hour">
+            {{ hour > 9 ? hour : ('0' + hour) }}
+        </div>
+        <span :class="['ticker', isTiming ? 'tick' : '']">:</span>
+        <div class="min">
+            {{ min > 9 ? min : ('0' + min) }}
+        </div>
+        <span :class="['ticker', isTiming ? 'tick' : '']">:</span>
+        <div class="sec">
+            {{ sec > 9 ? sec : ('0' + sec) }}
+        </div>
+        <span :class="['ticker', isTiming ? 'tick' : '']">.</span>
+        <div class="dot-sec">
+            {{ dot_sec }}
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.tick-timer {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    font-size: 2rem;
+    font-family: UniDream-LED;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .ticker {
+        margin: 0 1px;
+    }
+
+    .tick {
+        animation: tick-tick 1s infinite;
+        @keyframes tick-tick {
+            0% {
+                opacity: 0;
+            }
+            50% {
+                opacity: 0;
+            }
+            51% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+    }
+}
+</style>
