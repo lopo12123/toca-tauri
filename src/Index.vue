1edb7e19e71bf1c85d53450f7320635875094f11
lopo <lopo@zju.edu.cn>
2022-07-12 13:20:56 +0800

    feat: 布局

--- src/Index.vue
+++ src/Index.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
+import GlobalBanner from "@/layouts/GlobalBanner.vue";
+import useMemo from "@/stores/useMemo";
+
+const configMemo = useMemo()
 
 // 阻止右键响应
 onMounted(() => {
@@ -15,23 +19,48 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="index global">
-        <router-view/>
+    <div class="index global" :style="`opacity: ${configMemo.globalAlpha}`">
+        <div class="banner-container">
+            <GlobalBanner/>
+        </div>
+        <div class="body-container">
+            <router-view/>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use "src/styles/mixin";
+
 .index {
+    @include mixin.scrollBarStyle(var(--scroll-bar-color));
     position: relative;
-    width: calc(100vw - 4px);
-    max-width: calc(100vw - 4px);
-    height: calc(100vh - 4px);
-    max-height: calc(100vh - 4px);
-    border: solid 2px var(--separator-stroke);
+    //width: calc(100vw - 4px);
+    //max-width: calc(100vw - 4px);
+    //height: calc(100vh - 4px);
+    //max-height: calc(100vh - 4px);
+    width: 300px;
+    height: 100px;
+    border: solid 2px var(--border-color);
+    background-color: var(--background-color);
     color: var(--text-color);
-    overflow: auto;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+    overflow: auto;
+
+    .banner-container {
+        position: relative;
+        width: 100%;
+        height: 1.5rem;
+        border-bottom: solid 1px var(--border-color);
+    }
+
+    .body-container {
+        position: relative;
+        width: 100%;
+        height: calc(100% - 1.5rem - 1px);
+    }
 }
 </style>
