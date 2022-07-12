8c6ae2fc5590080455ca763c8a9425547f5e86a2
lopo <lopo@zju.edu.cn>
2022-07-12 15:23:12 +0800

    feat: 首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,22 @@
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
-import { appWindow } from "@tauri-apps/api/window";
 
 const router = useRouter()
 
-const useTools = (type: 'min' | 'max' | 'refresh' | 'exit') => {
-    switch(type) {
-        case "min":
-            appWindow
-                .minimize()
-                .catch(err => {
-                    console.log('最小化出错')
-                })
-            break
-        case "max":
-            appWindow
-                .isMaximized()
-                .then((isMax) => {
-                    return isMax
-                        ? appWindow.unmaximize()
-                        : appWindow.maximize()
-                })
-                .catch(err => {
-                    console.log('最大化出错')
-                })
-            break
-        case "refresh":
-            window.location.reload()
-            break
-        case "exit":
-            appWindow
-                .close()
-                .catch(err => {
-                    console.log(err)
-                })
-            break
-    }
-}
-
-const selectOption = (type: 'display' | 'record') => {
-    if(type === 'display') {
-        router.push({ name: 'DisplayView' })
-    }
-    else if(type === 'record') {
-        router.push({ name: 'RecordView' })
-    }
+const makeChoice = (choice: 'record' | 'display') => {
+    if(choice === 'record') router.push({ name: 'RecordView' })
+    else if(choice === 'display') router.push({ name: 'DisplayView' })
 }
 </script>
 
 <template>
     <div class="home">
-        <div class="side-menu">
-            <div class="min" @click="useTools('min')">
-                1
-            </div>
-            <div class="max" @click="useTools('max')">
-                2
-            </div>
-            <div class="refresh" @click="useTools('refresh')">
-                3
-            </div>
-            <div class="exit" @click="useTools('exit')">
-                4
-            </div>
+        <div class="logo">
+            logo
         </div>
         <div class="options">
-            <div class="option-btn" @click="selectOption('display')">播放</div>
-            <div class="option-btn" @click="selectOption('record')">录制</div>
+            <div class="option-btn" @click="makeChoice('record')">录制</div>
+            <div class="option-btn" @click="makeChoice('display')">播放</div>
         </div>
     </div>
 </template>
@@ -76,24 +26,36 @@ const selectOption = (type: 'display' | 'record') => {
 
 .home {
     position: relative;
-    width: 100%;
-    height: 100%;
+    width: calc(100% - 1rem);
+    height: calc(100% - 1rem);
+    padding: 0.5rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
 
-    .option-btn {
-        width: 5rem;
-        height: 3rem;
-        //border: solid 1px var(--separator-stroke);
-        //user-select: none;
-        cursor: pointer;
+    .options {
+        position: relative;
+        height: 100%;
         display: flex;
+        flex-direction: column;
         align-items: center;
-        justify-content: center;
+        justify-content: space-between;
 
-        &:hover {
-            text-decoration: underline;
+        .option-btn {
+            position: relative;
+            width: 5rem;
+            height: 1.5rem;
+            border: solid 1px var(--border-color);
+            border-radius: 2px;
+            user-select: none;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            &:hover {
+                background-color: var(--background-color__dark);
+            }
         }
     }
 }
