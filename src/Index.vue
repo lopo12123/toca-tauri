332a62c2d7b7fd875b26e0f2d91bba4e4f032cdb
lopo <lopo@zju.edu.cn>
2022-07-13 09:31:42 +0800

    style: 百分比

--- src/Index.vue
+++ src/Index.vue
@@ -43,12 +43,10 @@ onMounted(() => {
 .index {
     @include mixin.scrollBarStyle(var(--scroll-bar-color));
     position: relative;
-    width: calc(100vw - 4px);
-    max-width: calc(100vw - 4px);
-    height: calc(100vh - 4px);
-    max-height: calc(100vh - 4px);
-    //width: 300px;
-    //height: 100px;
+    width: calc(100% - 4px);
+    max-width: calc(100% - 4px);
+    height: calc(100% - 4px);
+    max-height: calc(100% - 4px);
     border: solid 2px var(--border-color);
     border-radius: 5px;
     background-color: var(--background-color);
