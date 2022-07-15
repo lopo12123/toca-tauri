f0b6298966ea2041be6e192a5c8529b4d8080caa
lopo <lopo@zju.edu.cn>
2022-07-15 17:24:47 +0800

    feat: i18n

--- src-docs/views/Docs.vue
+++ src-docs/views/Docs.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 
+import { ref } from "vue";
+
+const lang = ref<'zh' | 'en'>('zh')
+const content = ref()
 </script>
 
 <template>
@@ -15,5 +19,15 @@
     position: relative;
     width: 100%;
     height: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+
+    .body-container {
+        width: 80vw;
+        height: fit-content;
+        min-height: calc(100% - 100px);
+        outline: solid 1px red;
+    }
 }
 </style>
